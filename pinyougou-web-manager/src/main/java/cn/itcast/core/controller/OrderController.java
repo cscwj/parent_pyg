28ec1e24bd834e0d5e082a3ed126e35394787dc8
xxx <123@163.com>
2018-12-28 14:37:20 +0800

    测试push

---
+++ pinyougou-web-manager/src/main/java/cn/itcast/core/controller/OrderController.java
@@ -0,0 +1,57 @@
+package cn.itcast.core.controller;
+
+
+import cn.itcast.core.pojo.good.Goods;
+import cn.itcast.core.pojo.order.Order;
+import cn.itcast.core.service.OrderService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import entity.PageResult;
+import entity.Result;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pojogroup.OrderVo;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Reference
+    private OrderService orderService;
+
+    @RequestMapping("/search")
+    public PageResult search(Integer pageNum, Integer pageSize , @RequestBody Order order){
+
+            PageResult search = orderService.search(pageNum, pageSize, order);
+            List<Order> rows = search.getRows();
+            for (Order row : rows) {
+                System.out.println(row);
+            }
+        Field[] declaredFields = search.getRows().get(0).getClass().getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            System.out.println(declaredField+"="+declaredField.getGenericType());
+        }
+            return orderService.search(pageNum,pageSize,order);
+
+    }
+
+
+    @RequestMapping("/findOne")
+    public OrderVo findOne(Long id){
+        return orderService.findOne(id);
+    }
+
+    @RequestMapping("/update")
+    public Result update(@RequestBody OrderVo orderVo){
+        try {
+            orderService.update(orderVo);
+            return new Result(true,"更改订单成功");
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,"更改订单失败");
+        }
+    }
+}
---
+++ pinyougou-web-manager/src/main/webapp/js/controller/orderController.js
@@ -0,0 +1,76 @@
+4// 定义控制器:
+app.controller("orderController",function($scope,$controller,$http,orderService){
+	// AngularJS中的继承:伪继承
+	$controller('baseController',{$scope:$scope});
+	
+	// 查询所有的品牌列表的方法:
+	$scope.findAll = function(){
+		// 向后台发送请求:
+        orderService.findAll().success(function(response){
+			$scope.list = response;
+		});
+	}
+
+	// 分页查询
+	$scope.findPage = function(page,rows){
+		// 向后台发送请求获取数据:
+        orderService.findPage(page,rows).success(function(response){
+			$scope.paginationConf.totalItems = response.total;
+			$scope.list = response.rows;
+		});
+	}
+	
+	// 保存品牌的方法:
+	$scope.update = function(){
+
+        orderService.update($scope.entity).success(function(response){
+			// {flag:true,message:xxx}
+			// 判断保存是否成功:
+			if(response.flag){
+				// 保存成功
+				alert(response.message);
+				$scope.reloadList();
+			}else{
+				// 保存失败
+				alert(response.message);
+			}
+		});
+	}
+	
+	// 查询一个:
+	$scope.findById = function(id){
+        orderService.findOne(id).success(function(response){
+			// {id:xx,name:yy,firstChar:zz}
+			$scope.entity = response;
+		});
+	}
+	
+	// 删除品牌:
+	$scope.dele = function(){
+        orderService.dele($scope.selectIds).success(function(response){
+			// 判断保存是否成功:
+			if(response.flag==true){
+				// 保存成功
+				// alert(response.message);
+				$scope.reloadList();
+				$scope.selectIds = [];
+			}else{
+				// 保存失败
+				alert(response.message);
+			}
+		});
+	}
+	
+	$scope.searchEntity={};
+	
+	// 假设定义一个查询的实体：searchEntity
+	$scope.search = function(page,rows){
+		// 向后台发送请求获取数据:
+        orderService.search(page,rows,$scope.searchEntity).success(function(response){
+			$scope.paginationConf.totalItems = response.total;
+			$scope.list = response.rows;
+
+		});
+	}
+	
+});
