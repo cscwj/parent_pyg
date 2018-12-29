1079f410820ce0bfdbbb42bba1a89ffb560af883
nighrain <wangzsjob@163.com>
2018-12-29 20:20:03 +0800

    测试导出Excel

---
+++ pinyougou-interface/src/main/java/cn/itcast/core/service/UserExportService.java
@@ -0,0 +1,13 @@
+package cn.itcast.core.service;
+
+//Created by Wang on itCast.  
+
+/**
+ * @author Wang
+ * @version 1.0
+ * -description lianxi
+ * -date        2018-12-29--17:00 @_@ ~~
+ */
+
+public interface UserExportService {
+}
---
+++ pinyougou-service-user/src/main/java/cn/itcast/core/service/UserExportServiceImpl.java
@@ -0,0 +1,13 @@
+package cn.itcast.core.service;
+
+//Created by Wang on itCast.  
+
+/**
+ * @author Wang
+ * @version 1.0
+ * -description step :
+ * -date        2018-12-29--16:59 @_@ ~~
+ */
+
+public class UserExportServiceImpl implements  UserExportService{
+}
